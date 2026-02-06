3962dff963e9d82b681d1f4b46aa2af6df494bf1
vinoth9598 <pvinoth9598@gmail.com>
2026-02-06 16:52:32 +0530

    feature/UC6-Game_Restarts_Exit

--- src/GuessingApp.java
+++ src/GuessingApp.java
@@ -3,46 +3,52 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessingApp {
-    public static void main(String[] args) throws InvalidInputException{
+    static void main(String[] args) throws InvalidInputException {
         Scanner sc = new Scanner(System.in);
-
+        boolean restart = false;
         System.out.println("++++++++++++++++++++++++");
         System.out.println("Welcome to Guessing App");
         System.out.println("+++++++++++++++++++++++++\n");
-        System.out.print("Enter Player Name :");
-        String player = sc.nextLine();
 
-        GameConfig gameConfig = new GameConfig();
-        gameConfig.showRules();
 
-        int attempts = 0;
-        int minHint = 0;
+        do {
+            System.out.print("Enter Player Name :");
+            String player = sc.nextLine();
 
-        boolean win = false ;
+
+            GameConfig gameConfig = new GameConfig();
+            gameConfig.showRules();
+
+            int attempts = 0;
+            int minHint = 0;
+
+            boolean win = false;
+
 // Exhausts the maximum attempts
-        while (attempts < gameConfig.getMax_Attempts()){
-            System.out.print("Enter Your guess :");
+            while (attempts < gameConfig.getMax_Attempts()) {
+                System.out.print("Enter Your guess :");
 //            user input validation before
-            int guess = ValidationService.validateInput(sc.nextLine());
-            attempts++ ;
-            minHint++ ;
+                int guess = ValidationService.validateInput(sc.nextLine());
+                attempts++;
+                minHint++;
 
-            String result = GuessValidator.validateGuess(guess, gameConfig.getTargetNumber());
-            System.out.println(result);
+                String result = GuessValidator.validateGuess(guess, gameConfig.getTargetNumber());
+                System.out.println(result);
 
-            String result1 = HintService.generateHint(guess, minHint);
-            System.out.println(result1);
+                String result1 = HintService.generateHint(guess, minHint);
+                System.out.println(result1);
 
-            if("Correct".equals(result)){
-                break;
-            }
+                if ("Correct".equals(result)) {
+                    break;
+                }
 
 //            after the game loop completes
-            StorageService.saveResult(player, attempts, win);
+                StorageService.saveResult(player, attempts, win);
 
-        }
+                restart = GameController.restartGame(sc);
+            }
 
 
-
+        } while (restart);
     }
 }
