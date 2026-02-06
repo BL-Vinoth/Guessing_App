5091fc3f5b14afb9caf0a17ee0556c0982303431
vinoth9598 <pvinoth9598@gmail.com>
2026-02-06 16:09:39 +0530

    feature/UC5-Game_Result_Storage

---
+++ src/StorageService.java
@@ -0,0 +1,22 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class StorageService {
+
+    public static void saveResult(String player, int attempts , boolean win){
+//        after the operation completes
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("game_results.txt", true))){
+
+            writer.write("Player :" + player +
+                    ", Attempts :" + attempts +
+                    ", Result :" + (win ? "Win" : "Lose"));
+            writer.newLine();
+
+        }catch(IOException e){
+            System.out.println("Unable to save game result");
+        }
+    }
+
+
+}
